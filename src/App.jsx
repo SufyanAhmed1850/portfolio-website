2beb846aaf438a230676b2f52ff3665d19c795be
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2023-11-16 20:12:39 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./Components/Hero";
+
+function App() {
+    return (
+        <main>
+            <Hero />
+        </main>
+    );
+}
+
+export default App;
---
+++ src/Components/Hero/index.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+    return (
+        <div>
+            <div>
+                <h1>Nice to meet you!</h1>
+                <h1>
+                    I’m <span>Sufyan Ahmed.</span>
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
