3c283b20c9cdb5588713e651b3a6c6f3f83b1ec3
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2023-11-18 20:00:16 +0500

    projects overlay

--- src/Components/Projects/Project/index.jsx
+++ src/Components/Projects/Project/index.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import "./project.css";
 
 const Project = ({ img, title, techs }) => {
+    const [showOverlay, setShowOverlay] = useState(false);
+
     return (
-        <div className="project">
-            <img src={img} alt={title} />
-            <h3>{title}</h3>
-            <div className="tech">
-                {techs.map((tech, ind) => {
-                    return <p key={ind}>{tech}</p>;
-                })}
+        <div
+            onMouseEnter={() => setShowOverlay(true)}
+            onMouseLeave={() => setShowOverlay(false)}
+            className={`project ${showOverlay ? "overlay-active" : ""}`}
+        >
+            <div className="img-container">
+                <img src={img} alt={title} />
+                <div className="overlay">
+                    <div className="overlay-btn">
+                        <div className="btn">
+                            <span>VIEW PROJECT</span>
+                        </div>
+                        <div className="btn">
+                            <span>VIEW CODE</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h3>{title}</h3>
+                <div className="tech">
+                    {techs.map((tech, ind) => {
+                        return <p key={ind}>{tech}</p>;
+                    })}
+                </div>
             </div>
         </div>
     );
