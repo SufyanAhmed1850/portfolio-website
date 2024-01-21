929254627ae87179e7307ca6a6e2f70d5c878b0e
SufyanAhmed1850 <sufyan.ahmed1850@gmail.com>
2024-01-21 11:37:01 +0500

    favicon synced with theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CursorContext } from "./context/CursorContext";
 import { motion } from "framer-motion";
 import Home from "@pages/Home";
 
 function App() {
     const { cursorVariants, cursorBg, isVisible } = useContext(CursorContext);
+    const [systemTheme, setSystemTheme] = useState(
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light",
+    );
+    useEffect(() => {
+        const favicon = document.getElementById("favicon");
+        if (favicon) {
+            favicon.href =
+                systemTheme === "dark"
+                    ? "./src/assets/favicon-light.svg"
+                    : "./src/assets/favicon-dark.svg";
+        }
+    }, [systemTheme]);
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        const handleChange = () => {
+            setSystemTheme(mediaQuery.matches ? "dark" : "light");
+        };
+        mediaQuery.addEventListener("change", handleChange);
+        return () => {
+            mediaQuery.removeEventListener("change", handleChange);
+        };
+    }, []);
     return (
         <>
             <Home />
